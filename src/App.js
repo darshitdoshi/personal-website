3f48df29d72aac8ab7118d682d3863b66791199d
Darshit <darshit.doshi@gmail.com>
2022-03-06 05:07:05 -0800

    changes

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import {
+  navBar,
+  mainBody,
+  about,
+  repos,
+  leadership,
+  skills,
+  getInTouch,
+  experiences
+} from "./editable-stuff/config.js";
+import MainBody from "./components/home/MainBody";
+import AboutMe from "./components/home/AboutMe";
+import Project from "./components/home/Project";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+// import { Blog } from "./components/blog/Blog";
+// import BlogPost from "./components/blog/BlogPost";
+import GetInTouch from "./components/home/GetInTouch.jsx";
+import Leadership from "./components/home/Leadership.jsx";
+
+import Experience from "./components/home/Experience";
+
+const Home = React.forwardRef((props, ref) => {
+  return (
+    <>
+      <MainBody
+        gradient={mainBody.gradientColors}
+        title={`${mainBody.firstName} ${mainBody.middleName} ${mainBody.lastName}`}
+        message={mainBody.message}
+        icons={mainBody.icons}
+        link={mainBody.imageLink}
+        imgSizeHeight={mainBody.imageSizeHeight}
+        imgSizeWidth={mainBody.imageSizeWidth}
+        getInTouchMessage={mainBody.getInTouchMessage}
+        email={mainBody.email}
+        ref={ref}
+      />
+      {about.show && (
+        <AboutMe
+          heading={about.heading}
+          message={about.message}
+          link={about.imageLink}
+          imgSizeHeight={about.imageSizeHeight}
+          imgSizeWidth={about.imageSizeWidth}
+          resume={about.resume}
+        />
+      )}
+      {
+        experiences.show && (
+          <Experience experiences={experiences}/>
+        )
+      }
+      {repos.show && (
+        <Project
+          heading={repos.heading}
+          username={repos.gitHubUsername}
+          length={repos.reposLength}
+          specfic={repos.specificRepos}
+        />
+      )}
+      {leadership.show && (
+        <Leadership
+          heading={leadership.heading}
+          message={leadership.message}
+          img={leadership.images}
+          imageSize={leadership.imageSize}
+        />
+      )}
+    </>
+  );
+});
+
+const App = () => {
+  const titleRef = React.useRef();
+
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
+      {navBar.show && <Navbar ref={titleRef} />}
+      <Route path="/" exact component={() => <Home ref={titleRef} />} />
+      {/* {false && <Route path="/blog" exact component={Blog} />}
+      {false && <Route path="/blog/:id" component={BlogPost} />} */}
+    </BrowserRouter>
+  );
+};
+
+export default App;
