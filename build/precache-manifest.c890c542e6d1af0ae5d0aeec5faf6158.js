8d83ee9579696440735f36be3ac121b179e3c241
Darshit <darshit.doshi@gmail.com>
2022-03-06 16:12:58 -0800

    changes

---
+++ build/precache-manifest.c890c542e6d1af0ae5d0aeec5faf6158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de980276927d9818cfe7a6987d890ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdd4a6e3fb01a602b715",
+    "url": "/static/css/main.e9bdb83d.chunk.css"
+  },
+  {
+    "revision": "dd31c148f83ed8bb15bb",
+    "url": "/static/js/2.6f9b099d.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.6f9b099d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd4a6e3fb01a602b715",
+    "url": "/static/js/main.8ae6eca1.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "b175827f0f603fefcb84c23ff2d0f488",
+    "url": "/static/media/darshit.b175827f.png"
+  }
+]);
